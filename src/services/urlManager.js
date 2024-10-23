245f216899d78f3e4f9c6c28fe7c292e085cb3dc
Tzans <chudanhtuan2034@gmail.com>
2024-10-23 19:47:23 +0700

    no message

--- src/services/urlManager.js
+++ src/services/urlManager.js
@@ -10,6 +10,13 @@ const urlManager = {
     updateCategory: (id) => `/categories/${id}`, // Cập nhật danh mục với ID
     deleteCategory: (id) => `/categories/${id}`, // Xóa danh mục với ID
     searchCategoriesByName: '/categories/search', // Tìm kiếm danh mục theo tên
+
+    // Thêm các endpoint cho sách
+    getAllBooks: '/books', // Lấy tất cả sách
+    createBook: '/books', // Tạo sách mới
+    updateBook: (id) => `/books/${id}`, // Cập nhật sách với ID
+    deleteBook: (id) => `/books/${id}`, // Xóa sách với ID
+    uploadImage: '/books/upload', // Endpoint để tải lên hình ảnh
 };
 
 export default urlManager;
